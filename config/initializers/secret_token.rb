faf1b8f9be58346e3fd8a371a877f3de8c5beea9
tomohitoy <t07840ty@gmail.com>
2013-06-01 11:44:54 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melody::Application.config.secret_token = '24c3b82679fd9639fa8a0f3b289997a9733d38fabd416fb0c7e8bb6d945fcb0de0837ba8923dcd8aefb97443cd08967c17fb9792a85c0b9741727da4fb914c2a'
