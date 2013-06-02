1ab84b08385ce6612f4114519785476f4f20d5bf
tomohitoy <t07840ty@gmail.com>
2013-06-02 13:27:36 +0900

    update

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -1,7 +1,5 @@
 class Checkpoint < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :mission_id, :point, :pointnum, :title, :state
-  geocoded_by :address
-  after_validation :geocode
   belongs_to :mission
   has_one :milestone
 end
