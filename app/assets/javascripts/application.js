4dd3ac908ccf1388c5f6844518820748025c73d4
tomohitoy <t07840ty@gmail.com>
2013-06-02 09:49:24 +0900

    delete jquery-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery_ujs
 //= require main
 //= require vendor/bootstrap.min
 //= require vendor/modernizr-2.6.2-respond-1.1.0.min
